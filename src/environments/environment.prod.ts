56215300376530c7f7a111e0d427b79db651d94e
VincentStr <89853478+VincentStr@users.noreply.github.com>
2022-08-23 15:57:48 +0200

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  weatherApiBaseUrl: "https://yahoo-weather5.p.rapidapi.com/weather",
-  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue: 'yahoo-weather5.p.rapidapi.com',
-  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-  XRapidAPIKeyHeaderValue: 'aa2175fad0msh0700173bdc32f7fp138f69jsn38695ebc6d36'
-};
